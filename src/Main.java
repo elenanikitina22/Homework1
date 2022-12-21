15bb4912efc1e3a1f64ad6797b5ee45b77ffba85
Elena Nikitina <elena_nikitina_86@inbox.ru>
2022-12-21 23:07:58 +0300

    Домашнее задание Методы объектов

--- src/Main.java
+++ src/Main.java
@@ -22,23 +22,17 @@ public class Main {
         // toString
 
         System.out.println("book1.toString() = " + book1.toString());
-        System.out.println("book2.toString() = " + book2.toString());
         System.out.println("author1.toString() = " + author1.toString());
-        System.out.println("author2.toString() = " + author2.toString());
 
         // hashCode
 
         System.out.println("book1.hashCode() = " + book1.hashCode());
-        System.out.println("book2.hashCode() = " + book2.hashCode());
         System.out.println("author1.hashCode() = " + author1.hashCode());
-        System.out.println("author2.hashCode() = " + author2.hashCode());
 
         // equals
 
         System.out.println("book1.equals(book1) = " + book1.equals(book1));
-        System.out.println("book1.equals(book1) = " + book1.equals(book2));
-        System.out.println("author1.equals(book1) = " + author1.equals(book1));
-        System.out.println("author2.equals(author2) = " + author2.equals(author2));
+        System.out.println("author1.equals(author2) = " + author1.equals(author2));
 
     }
 }
