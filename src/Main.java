916bca3aabe86a15bc7c2584d5d151694a79c3cd
Elena Nikitina <elena_nikitina_86@inbox.ru>
2022-12-25 16:15:07 +0300

    Коммит

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задание 1
+        // Билет 7
+
+        // Когда применяется цикл while?
+        // Для чего применяется метод toString?
+        // Что такое область видимости?
+
+        //Задание 1
+
+        // Напишите метод, который возводит число в квадрат.
 
         System.out.println("Hello world!");
 
