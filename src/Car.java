d47e80e8902a40aebccd02a7f2cfdfccb669290f
Elena Nikitina <elena_nikitina_86@inbox.ru>
2022-12-27 19:02:38 +0300

    Домашнее задание ООП. Часть 1. Введение

--- src/Car.java
+++ src/Car.java
@@ -1,29 +1,56 @@
+import java.util.Objects;
+
 public class Car {
     String brand;
     String model;
-    int engineVolume;
+    double engineVolume;
     String color;
     int productionYear;
     String productionCountry;
 
-    public Car(String brand, String model, int engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionYear = productionYear;
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+
+        if (Objects.equals(brand, "") || Objects.equals(brand, "null")) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+
+        if (Objects.equals(model, "") || Objects.equals(model, "null")) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+
+        if (Objects.equals(color, "") || Objects.equals(color, "null")) {
+            this.color = "Белый";
+        } else {
+            this.color = color;
+        }
+
+        if (productionYear <= 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+
         this.productionCountry = productionCountry;
     }
 
     @Override
     public String toString() {
-        return "Car{" +
-                "brand='" + brand + '\'' +
-                ", model='" + model + '\'' +
-                ", engineVolume=" + engineVolume +
-                ", color='" + color + '\'' +
-                ", productionYear=" + productionYear +
-                ", productionCountry='" + productionCountry + '\'' +
-                '}';
+        return "Машина " +
+                "Бренд " + brand +
+                ", Модель " + model +
+                ", Объем двигателя " + engineVolume +
+                ", Цвет " + color +
+                ", Год выпуска " + productionYear +
+                ", Сборка " + productionCountry;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-
-        // Задание 1
-
-        // Car car1 = new Car("a",);
-
-
-
-        // Задание 2
-
-
-
-
-        // Задание 3
-
-
-
-
-
-
-
+        task();
     }
+
+    // Создание записей о 5 машинах
+
+    private static final Car[] cars = {
+            new Car("Lada", "Granta", 1.7, "Желтый", 2015, "Россия"),
+            new Car("Audi ", "A8 50 L TDI quattro", 3.0, "Черный", 2020, "Германия"),
+            new Car("BMW", "Z8", 3.0, "Черный", 2021, "Германия"),
+            new Car("Kia", "Sportage", 2.4, "Красный", 2018, "Южная Корея"),
+            new Car("Hyundai", "Avante", 1.6, "Оранжевый", 2016, "Южная Корея"),
+    };
+
+    // Список всех машин
+
+    public static void task() {
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+    }
+
 }
