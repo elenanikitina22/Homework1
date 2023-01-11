9457d1c085f03fe0c554a2e3e4cbc6b89d3401a1
Elena Nikitina <elena_nikitina_86@inbox.ru>
2023-01-11 09:07:31 +0300

    Домашнее задание ООП. Часть 4. Полиморфизм

--- src/Main.java
+++ src/Main.java
@@ -34,16 +34,13 @@ public class Main {
 
     public static void task1() {
 
-        //DriverTypeB driverB1 = new DriverTypeB("Денисов Тимофей Львович", "B", 2);
-        //DriverTypeC driverC1 = new DriverTypeC("Иванов Мирон Фёдорович ", "C", 4);
-        //DriverTypeD driverD1 = new DriverTypeD("Морозова Валерия Германовна", "D", 6);
-       // DriverTypeB driverB2 = new DriverTypeB("Золотарев Михаил Дмитриевич", "B", 8);
+        DriverTypeB driverB1 = new DriverTypeB("Денисов Тимофей Львович", "B", 2);
+        DriverTypeC driverC1 = new DriverTypeC("Иванов Мирон Фёдорович ", "C", 4);
+        DriverTypeD driverD1 = new DriverTypeD("Морозова Валерия Германовна", "D", 6);
+        DriverTypeB driverB2 = new DriverTypeB("Золотарев Михаил Дмитриевич", "B", 8);
 
-      //  System.out.println("Водитель" + driverB1.getName() + "управляет автомобилем Б и будет участвовать в заезде");
+        System.out.println("Водитель " + driverB1.getName() + " управляет автомобилем " + " " + " и будет участвовать в заезде.");
 
     }
-
-    // Список всех легковых автомобилей
-
 
 }
