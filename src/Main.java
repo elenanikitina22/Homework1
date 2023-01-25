041c2c42e3f6b8596e45e29a0d9e230540534e51
Elena Nikitina <elena_nikitina_86@inbox.ru>
2023-01-25 20:25:58 +0300

    Курсовая работа 2-го курса

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -11,6 +13,12 @@ public class Main {
 
         // Задача на функциональное программирование
 
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите текст: ");
+        String text = in.nextLine();
+
+        System.out.printf("Ваш текст: %s \n", text);
+        in.close();
 
 
     }
