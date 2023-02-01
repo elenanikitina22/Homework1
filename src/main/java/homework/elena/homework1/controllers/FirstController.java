8061303b9e55eeda720ad5ff9d970a75fda9e755
Elena Nikitina <elena_nikitina_86@inbox.ru>
2023-02-01 22:18:40 +0300

    Коммит

---
+++ src/main/java/homework/elena/homework1/controllers/FirstController.java
@@ -0,0 +1,20 @@
+package homework.elena.homework1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/*")
+    public String applicationLaunched() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String infoPage() {
+        return "Елена Никитина, проект Homework2, 01.02.203, Учебный проект";
+    }
+
+}
