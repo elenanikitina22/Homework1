3bfc783c31119d20e3be0d13a49431a89dd461f2
Elena Nikitina <elena_nikitina_86@inbox.ru>
2023-03-09 22:01:56 +0300

    Коммит

--- src/main/java/homework/elena/homework1/Main.java
+++ src/main/java/homework/elena/homework1/Main.java
@@ -1,7 +1,5 @@
 package homework.elena.homework1;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
@@ -21,26 +19,26 @@ public class Main {
         });
 
         // Второе задание
-        GetNumberOfEvenNumbers(list.stream());
+        getNumberOfEvenNumbers(list.stream());
     }
 
     public static void findMinMax(Stream<? extends Integer> stream, Comparator<? super Integer> order, BiConsumer<? super Integer, ? super Integer> minMaxConsumer)
     {
-        Integer min = stream.min(order).get();
-        Integer max = stream.max(order).get();
-
-        if (min != null || max != null)
+        try
         {
+            Integer min = stream.min(order).get();
+            Integer max = stream.max(order).get();
             minMaxConsumer.accept(min, max);
         }
-        else
+        catch (NoSuchElementException ex)
         {
             minMaxConsumer.accept(null, null);
         }
     }
 
-    public static long GetNumberOfEvenNumbers(Stream<? extends Integer> stream)
+    public static void getNumberOfEvenNumbers(Stream<? extends Integer> stream)
     {
-        return stream.filter(e -> e % 2 == 0).peek(e -> System.out.println(e)).count();
+        long count = stream.filter(e -> e % 2 == 0).peek(e -> System.out.println(e)).count();
+        System.out.println("Количество - " + count);
     }
 }
