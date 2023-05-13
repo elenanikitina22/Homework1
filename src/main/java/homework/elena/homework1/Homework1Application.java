0e381695d9630acaa827680f8ae5f4ce97384ea5
Elena Nikitina <elena_nikitina_86@inbox.ru>
2023-05-13 13:44:52 +0300

    Коммит

--- src/main/java/homework/elena/homework1/Homework1Application.java
+++ src/main/java/homework/elena/homework1/Homework1Application.java
@@ -3,11 +3,24 @@ package homework.elena.homework1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class Homework1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Homework1Application.class, args);
+
+        String text = "Текст для разделения на части.";
+        String delimiter = " ";
+
+        // Разделение строки
+        String[] parts = text.split(delimiter);
+
+        // Вывод второго слова
+        System.out.println("Второе слово: " + parts[1]);
+
+        // SpringApplication.run(Homework1Application.class, args);
+
     }
 
 }
